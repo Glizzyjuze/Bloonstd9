83b3f941caf2b13b7ec7097f02085cc15cf227da
Hugo Zingmark <Hugo.priebezingmark2@gmail.com>
2022-10-13 15:33:18 +0200

    first commit

---
+++ Bloonstd9/Program.cs
@@ -0,0 +1,37 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(1024, 768, "Topdown game");
+Raylib.SetTargetFPS(60);
+
+Color myColor = new Color(255, 120, 60, 255);
+
+Rectangle character = new Rectangle(0, 60, 50, 50);
+float speed = 5f;
+
+while (Raylib.WindowShouldClose() == false)
+{
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+        character.x += speed;
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+        character.x -= speed;
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    {
+        character.y -= speed;
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+        character.y += speed;
+    }
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLACK);
+    Raylib.DrawRectangleRec(character, myColor);
+    Raylib.EndDrawing();
+}
